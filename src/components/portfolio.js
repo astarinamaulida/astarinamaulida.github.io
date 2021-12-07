5da54720c166c909385c761f5e9824fa6c9fe934
Astarina Maulida <astarina.maulida@gmail.com>
2021-12-06 20:56:29 -0500

    Add components and portfolio

---
+++ src/components/portfolio.js
@@ -0,0 +1,97 @@
+const about = {
+  name: 'Astarina Maulida',
+  role: 'Fullstack Developer',
+  description:
+    'Having unique and complete perspectives, I have web development skills strengthened by my bachelor\'s in business management with finance, which provides logic with great business acumen. I learned my visual and artistic design from a fashion design diploma from Milan where I learned a lot about colour palette trends, proportions, product designs and more.',
+  resume: '',
+  social: {
+    linkedin: 'https://www.linkedin.com/in/astarinamaulida/',
+    github: 'https://github.com/astarinamaulida',
+  },
+}
+
+const projects = [
+  {
+    name: 'Paykids',
+    description:
+      'PayKids is a multi-page React application, that allows children to customise their own virtual debit cards and bank account user interface.',
+    stack: ['React', 'PostgreSQL', 'Node.js', 'Express', 'CSS3', 'Material-UI'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+  {
+    name: 'Cravings',
+    description:
+      'Cravings is a food pick-up ordering website for a cakery shop. The app uses Twilio as our API to implement SMS communication from the website to customers and the cakery shop',
+    stack: ['Node.js', 'Express.js', 'PostgreSQL', 'Javascript', 'EJS', 'jQuery', 'Bootstrap', 'SCSS'],
+    sourceCode: 'https://github.com/astarinamaulida/cravings',
+    livePreview: 'https://github.com/astarinamaulida/cravings',
+  },
+  {
+    name: 'Interview Scheduler',
+    description:
+      'Scheduler is a simple appointment scheduling app. The frontend is React and the backend is Node.js. The storybook was built for each components and the app is tested by using Jest for its unit testing. Cypress is used for automated testing',
+    stack: ['React', 'PostgreSQL','Javascript', 'Axios', 'Express.js', 'Classnames', 'Normalize', 'Storybook', 'Jest', 'Cypress', 'HTML5', 'CSS3' ],
+    sourceCode: 'https://github.com/astarinamaulida/scheduler',
+    livePreview: 'https://github.com/astarinamaulida/scheduler',
+  },
+  {
+    name: 'Tweeter',
+    description:
+      'Tweeter is a Single Page App (SPA) twitter clone project.',
+    stack: ['Node.js', 'Express.js','PostgreSQL', 'jQuery', 'HTML5', 'CSS3'],
+    sourceCode: 'https://github.com/astarinamaulida/tweeter',
+    livePreview: 'https://github.com/astarinamaulida/tweeter',
+  },
+  {
+    name: 'Jungle Rails',
+    description:
+      'Jungle rails is an e-commerce platform built with Ruby on Rails. This app uses Rspec for the unit testing and Capybara and Poltergeist. For the API, this app uses Stripe for the payment',
+    stack: ['Ruby on Rails', 'bcrypt','PostgreSQL', 'Bootstrap', 'Rspec','Capybara', 'Poltergeist', 'HTML5', 'SCSS'],
+    sourceCode: 'https://github.com/astarinamaulida/jungle-rails',
+    livePreview: 'https://github.com/astarinamaulida/jungle-rails',
+  },
+  {
+    name: 'TinyApp',
+    description:
+      'Tinyapp is an app that allows users to shorten long URLs. It is similar to bit.ly and TinyURL.',
+    stack: ['Node.js', 'Express.js','EJS', 'bcrypt', 'HTML5', 'CSS3'],
+    sourceCode: 'https://github.com/astarinamaulida/tinyapp',
+    livePreview: 'https://github.com/astarinamaulida/tinyapp',
+  },
+]
+
+const languages = [
+  'English',
+  'Indonesian',
+]
+
+const skills = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'TypeScript',
+  'React',
+  'Ruby',
+  'Node',
+  'Ajax',
+  'Express',
+  'EJS',
+  'jQuery',
+  'Bootstrap',
+  'SASS',
+  'Material UI',
+  'Git',
+  'PostgreSQL',
+  'Jest',
+  'Mocha',
+  'Adobe Photoshop',
+  'Canva',
+  'Figma',
+]
+
+const contact = {
+  email: 'astarina.maulida@mail.com',
+}
+
+export { about, projects, languages, skills, contact }
