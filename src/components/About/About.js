5f04b6686afcd3fbff7c9cf9113d4a8fec3f4e2d
Astarina Maulida <astarina.maulida@gmail.com>
2021-12-06 21:16:05 -0500

    Add about section

---
+++ src/components/About/About.js
@@ -0,0 +1,15 @@
+import { about } from "../../portfolio";
+
+export default function About() {
+  const { name, role, description, resume, social } = about;
+
+  return (
+    <div className="about">
+      <h1>
+        Hi, I am <span className="about-name">{name}.</span>
+      </h1>
+      <h2 className="about-role">{role}</h2>
+      <p className="about-desc">{description}</p>
+    </div>
+  );
+}
