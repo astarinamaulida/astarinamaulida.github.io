768b8b02ddff45edeae2b126b2b95d0184130e24
Astarina Maulida <astarina.maulida@gmail.com>
2021-12-08 06:55:24 -0500

    Add more stories in about section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,18 +1,23 @@
-import React from 'react'
+import React from "react";
 import { about } from "../../portfolio";
-import './About.css'
+import "./About.css";
 
 export default function About() {
-  const { name, role, description, resume, social } = about;
+  const { name, role, description, description2, description3, resume, social } = about;
 
   return (
     <div className="about">
-      <h1>
-        Hi, I am <span className="about-name">{name}.</span>
-      </h1>
-      
-      <h2 className="about-role">{role}</h2>
+      <div className="header">
+        <h1>Hi, I am <span className="about-name">{name}.</span>
+        <br/>
+        {role}</h1>
+        <img
+          className="profile-pic"src="https://github.com/astarinamaulida/astarinamaulida.github.io/blob/master/public/profilepic.png?raw=true"
+        />
+      </div>
       <p className="about-desc">{description}</p>
+      <p className="about-desc">{description2}</p>
+      <p className="about-desc">{description3}</p>
     </div>
   );
 }
--- src/portfolio.js
+++ src/portfolio.js
@@ -2,7 +2,11 @@ const about = {
   name: 'Astarina Maulida',
   role: 'A Fullstack Developer',
   description:
-    'Having unique and complete perspectives, I have web development skills strengthened by my bachelor\'s in business management with finance, which provides logic and business acumen.    I enhanced my visual and artistic design from a fashion design diploma from Milan where I learned a lot about colour palette trends, proportions, product designs and more.',
+    'I am new in the web development world, previously I was working in finance. I have been curious about coding. I was lucky to get a scholarship from the OWN (Ontario Web Developers) Initiative, which funded my study in Lighthouse Labs learning about web development. During the study, I fell in love more with code! Having lecturers and friends who are passionate about coding makes me want to develop more. Currently, I am learning typescript, gatsby, and plan to learn React Native.',
+  description2:
+    'I have unique perspectives as I have web development skills strengthened by my bachelor\'s in business management with finance, which provides logic and business acumen. I enhanced my visual and artistic design from a fashion design diploma from Milan where I learned a lot about colour palette trends, proportions, product designs and more.',
+  description3:
+    'Thanks for visiting my portfolio website, I hope you enjoy it!',
   resume: '',
   social: {
     linkedin: 'https://www.linkedin.com/in/astarinamaulida/',
