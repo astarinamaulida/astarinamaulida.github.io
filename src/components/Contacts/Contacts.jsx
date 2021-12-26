1212d334266e8aed7e66fe4d79a0585cf44f3be4
Astarina Maulida <astarina.maulida@gmail.com>
2021-12-26 17:55:02 -0500

    Add css and icons for contacts

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -6,15 +6,26 @@ export default function Contacts() {
   const { email, github, linkedin } = contacts;
 
   return (
-    <div className="contacts">
-      Contacts
-      <div className="header">
-      email: {email}
-      <br/>
-      github: {github}
-      <br/>
-     linkedin: {linkedin}
-    </div>
-    </div>
+    <section id="contacts" className="section-contacts">
+      <h2 className="contacts">Contacts</h2>
+      <div className="contacts-frame">
+        <div className="contacts-header">
+          <div className="header">
+            <div>email: {email}</div>
+            
+            <div>
+            <span>
+          <a href="https://github.com/astarinamaulida">
+            <img
+              className="github-logo-contacts"
+              src="https://github.com/astarinamaulida/astarinamaulida.github.io/blob/master/public/github-logo.png?raw=true"
+            ></img>
+          </a>
+        </span>{github}</div>
+            <div>linkedin: {linkedin}</div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
