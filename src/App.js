a2c29601270c00246e92cd6226494bce3265a709
Astarina Maulida <astarina.maulida@gmail.com>
2021-12-26 20:30:43 -0500

    Add content to the footer component and add css

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-import About from './components/About/About';
-import Project from './components/Projects/Project';
-import Navbar from './components/Nav/Navbar';
-import React from 'react';
-import Contacts from './components/Contacts/Contacts';
+import logo from "./logo.svg";
+import "./App.css";
+import About from "./components/About/About";
+import Project from "./components/Projects/Project";
+import Navbar from "./components/Nav/Navbar";
+import React from "react";
+import Contacts from "./components/Contacts/Contacts";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <React.Fragment>
       <Navbar />
-    <div className="App">
-      <div className="wrapper">
-      <About />
-      <Project />
-      <Contacts />
+      <div className="App">
+        <div className="wrapper">
+          <About />
+          <Project />
+          <Contacts />
+        </div>
+        <Footer />
       </div>
-    </div>
     </React.Fragment>
   );
 }
