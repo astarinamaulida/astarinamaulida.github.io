e93fe0b65faf1d4d48f22254a6f4a9cc34444e17
Astarina Maulida <astarina.maulida@gmail.com>
2021-12-27 07:01:16 -0500

    Add css to connect icon logos

--- src/components/Connect/Connect.jsx
+++ src/components/Connect/Connect.jsx
@@ -3,27 +3,48 @@ import { connect } from "../../portfolio";
 import "./Connect.css";
 
 export default function Connect() {
-  const { email, github, linkedin, instagram } = connect;
+  const { email, github, linkedin } = connect;
 
   return (
     <section id="connect" className="section-connect">
       <h2 className="connect">Connect</h2>
       <div className="connect-frame">
-        <div className="connect-header">
-        <span>
-          <a href="https://hijabtale.com">
-            <img
-              className="github-logo"
-              src="https://github.com/astarinamaulida/astarinamaulida.github.io/blob/master/public/github-logo.png?raw=true"
-            ></img>
-          </a>
-        </span>
-          <div className="connect-list">
-            <div>{email}</div>
-            <div>{github}</div>
-            <div>{linkedin}</div>
-            <div>{instagram}</div>
+        <div className="connect-icons">
+        <div>
+            <span>
+              <a href="mailto: astarina.maulida@gmail.com">
+                <img
+                  className="email-logo-connect"
+                  src="https://github.com/astarinamaulida/astarinamaulida.github.io/blob/master/public/toemail-logo.png?raw=true"
+                ></img>
+              </a>
+            </span>
           </div>
+          <div>
+            <span>
+              <a href="https://github.com/astarinamaulida">
+                <img
+                  className="github-logo-connect"
+                  src="https://github.com/astarinamaulida/astarinamaulida.github.io/blob/master/public/github-logo.png?raw=true"
+                ></img>
+              </a>
+            </span>
+          </div>
+          <div>
+            <span>
+              <a href="https://www.linkedin.com/in/astarinamaulida/">
+                <img
+                  className="linkedin-logo-connect"
+                  src="https://github.com/astarinamaulida/astarinamaulida.github.io/blob/master/public/linkedin-logo.png?raw=true"
+                ></img>
+              </a>
+            </span>
+          </div>
+        </div>
+        <div className="connect-list">
+          <div>{email}</div>
+          <div>{github}</div>
+          <div>{linkedin}</div>
         </div>
       </div>
     </section>
