2687662efc08be58ebad25bd2bc3fb3a3969a05c
Astarina Maulida <astarina.maulida@gmail.com>
2021-12-27 07:09:11 -0500

    Update description to match with current work

--- src/portfolio.js
+++ src/portfolio.js
@@ -2,16 +2,12 @@ const about = {
   name: 'Astarina Maulida',
   role: 'A Fullstack Developer',
   description:
-    'I am new in the web development world, previously I was working in finance. I have been curious about coding. I was lucky to get a scholarship from the OWN (Ontario Web Developers) Initiative, which funded my study in Lighthouse Labs learning about web development. During the study, I fell in love more with code! Having lecturers and friends who are passionate about coding makes me want to develop more. Currently, I am learning typescript, gatsby, and plan to learn React Native.',
+    'I am new in the web development world, previously I was working in finance. Currently, I have been working as a Software Engineer since December 2021. I have been curious about coding. I was lucky to get a scholarship from the OWN (Ontario Web Developers) Initiative, which funded my study in Lighthouse Labs learning about web development. During the study, I fell in love more with code! Having lecturers and friends who are passionate about coding makes me want to develop more. Currently, I am learning typescript, gatsby, C# and plan to learn React Native.',
   description2:
     'I have unique perspectives as I have web development skills strengthened by my bachelor\'s in business management with finance, which provides logic and business acumen. I enhanced my visual and artistic design from a fashion design diploma from Milan where I learned a lot about colour palette trends, proportions, product designs and more.',
   description3:
     'Thanks for visiting my portfolio website, I hope you enjoy it!',
   resume: '',
-  social: {
-    linkedin: 'https://www.linkedin.com/in/astarinamaulida/',
-    github: 'https://github.com/astarinamaulida',
-  },
 }
 
 const languages = [
