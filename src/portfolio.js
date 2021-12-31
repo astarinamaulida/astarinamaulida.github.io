c023afa0d2bade6e50f1173005d0eb9266e6b53c
Astarina Maulida <astarina.maulida@gmail.com>
2021-12-31 14:18:25 -0500

    Update new description for about component

--- src/portfolio.js
+++ src/portfolio.js
@@ -2,14 +2,13 @@ const about = {
   name: 'Astarina Maulida',
   role: 'A Fullstack Developer',
   description:
-    'I am new in the web development world. I have been working as a Software Engineer since December 2021. My main programming language is javascript with React as the library. However, I keep learning new stuff every day from typescript, gatsby, C# and plan to learn React Native.',
+    'I am new in the web development world. I have been working as a Software Engineer since December 2021. I enjoy coding in stacks like React, PostgreSQL, Express, Node. I acquired some coding languages both frontend and backend like Javascript, Ruby, HTML, CSS, and currently learning new things every day from typescript, gatsby, c# and planning to add React Native to my learning list. I love doing the visual creation, editing and adjustment with any image editor, like Adobe Photoshop, Canva, Adobe Illustrator also wireframe creation using Figma.',
   description2:
     'I have unique perspectives as I have web development skills strengthened by my bachelor\'s in business management with finance, which provides logic and business acumen. I enhanced my visual and artistic design from a fashion design diploma from Milan where I learned a lot about colour palette trends, proportions, product designs and more.',
   description3:
     'Apart from coding, I love books and enjoy my weekly library visit. I also love to travel and learn new languages, halo! ciao! bonjour! hola! kia ora! privet! salaam! anyeong haseyo! ni hao! konnichiwa! hello! hallo! namaste! Do not hesitate to reach me to have a chit chat in different languages, I love it! ❤️😊',
   description4:
     'Thanks for visiting my portfolio website, I hope you enjoy it!',
-  resume: '',
 }
 
 const languages = [
