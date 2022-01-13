70cac992239f464b6ab2679f77e7461981477362
Astarina Maulida <astarina.maulida@gmail.com>
2022-01-12 22:38:50 -0500

    Add keyboard double arrow up icon

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import Navbar from "./components/Nav/Navbar";
 import Connect from "./components/Connect/Connect";
 import Footer from "./components/Footer/Footer";
 import Skills from "./components/Skills/Skills";
+import KeyboardDoubleArrowUpIcon from '@mui/icons-material/KeyboardDoubleArrowUp';
 
 function App() {
   return (
     <React.Fragment>
       <Navbar />
+      <KeyboardDoubleArrowUpIcon/>
       <div className="App">
         <div className="wrapper">
           <About />
