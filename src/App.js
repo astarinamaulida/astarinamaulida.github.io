35b45c7e035dc822e1a7812a0f731a7a98984ad6
Astarina Maulida <astarina.maulida@gmail.com>
2022-08-20 00:26:25 -0400

    Reconstruct app, delete wrapper, adjust Nav

--- src/App.js
+++ src/App.js
@@ -1,35 +1,21 @@
 import React from "react";
-import "./App.css";
 import About from "./components/About/About";
 import Project from "./components/Projects/Project";
 import Navbar from "./components/Nav/Navbar";
 import Connect from "./components/Connect/Connect";
 import Footer from "./components/Footer/Footer";
 import Skills from "./components/Skills/Skills";
-import KeyboardDoubleArrowUpIcon from '@mui/icons-material/KeyboardDoubleArrowUp';
-import { styled } from '@mui/styles';
-import { bounce } from 'react-animations';
+import "./App.css";
 
 function App() {
-  // const DoubleArrow = styled(KeyboardDoubleArrowUpIcon)({
-  //   color: "#cbacac",
-  //   animation: "scrollBaby 80s linear infinite",
-  //   bounce: {
-  //     animationName: bounce,
-  //   }
-  // })
-
   return (
     <React.Fragment>
-      <Navbar />
-      {/* <DoubleArrow sx={{ fontSize: 50 }} color= "#cbacac" /> */}
       <div className="App">
-        <div className="wrapper">
-          <About />
-          <Project />
-          <Skills />
-          <Connect />
-        </div>
+        <Navbar />
+        <About />
+        <Project />
+        <Skills />
+        <Connect />
         <Footer />
       </div>
     </React.Fragment>
