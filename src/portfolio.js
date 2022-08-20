8b86f2abab3ee86419ed16f1ba69bbe84ea0e8fe
Astarina Maulida <astarina.maulida@gmail.com>
2022-08-20 00:15:10 -0400

    Add width to app

--- src/portfolio.js
+++ src/portfolio.js
@@ -2,7 +2,7 @@ const about = {
   name: 'Asta',
   role: 'a developer based in Canada.',
   description:
-    'I am new in the web development world. I have been working as a Software Engineer since December 2021. I enjoy coding in stacks like React, PostgreSQL, Express, Node. I acquired some coding languages both frontend and backend like Javascript, Ruby, HTML, CSS, and currently learning new things every day from typescript, gatsby, c#, tailwindcss and planning to add React Native to my learning list. I love doing the visual creation, editing and adjustment with any image editor, like Adobe Photoshop, Canva, Adobe Illustrator also wireframe creation using Figma.',
+    'I enjoy coding in stacks like React, PostgreSQL, Express, Node. I acquired some coding languages both frontend and backend like Javascript, Ruby, HTML, CSS, and currently learning new things every day from typescript, gatsby, c#, tailwindcss and planning to add React Native to my learning list. I love doing the visual creation, editing and adjustment with any image editor, like Adobe Photoshop, Canva, Adobe Illustrator also wireframe creation using Figma.',
   description2:
     'I have unique perspectives as I have web development skills strengthened by my bachelor\'s in business management with finance, which provides logic and business acumen. I enhanced my visual and artistic design from a fashion design diploma from Milan where I learned a lot about colour palette trends, proportions, product designs and more.',
   description3:
