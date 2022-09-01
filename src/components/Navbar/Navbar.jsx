3531b836fc96137ec461b83e56ff6afe06232cc3
Astarina Maulida <astarina.maulida@gmail.com>
2022-08-31 21:48:08 -0400

    Add name at Nav

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,63 +4,66 @@ import "./Navbar.css";
 
 export default function Navbar() {
   return (
-    <nav className="nav">
-      <LinkScroll
-        className="nav-link nav-about"
-        activeClass="active"
-        to="about"
-        spy="true"
-        smooth="true"
-        offset={-70}
-        duration={500}
-      >
-        About
-      </LinkScroll>
-      <LinkScroll
-        className="nav-link nav-projects"
-        activeClass="active"
-        to="projects"
-        spy={true}
-        smooth={true}
-        offset={-70}
-        duration={500}
-      >
-        Projects
-      </LinkScroll>
-      <LinkScroll
-        className="nav-link nav-wireframes"
-        activeClass="active"
-        to="wireframes"
-        spy={true}
-        smooth={true}
-        offset={-70}
-        duration={500}
-      >
-        Wireframes
-      </LinkScroll>
-      <LinkScroll
-        className="nav-link nav-skills"
-        activeClass="active"
-        to="skills"
-        spy={true}
-        smooth={true}
-        offset={-70}
-        duration={500}
-      >
-        Skills
-      </LinkScroll>
-      <a
-        className="nav-link nav-connect"
-        activeClass="active"
-        to="connect"
-        spy={true}
-        smooth={true}
-        offset={-70}
-        duration={500}
-        href="https://www.linkedin.com/in/astarinamaulida/"
-      >
-        Connect
-      </a>
+    <nav className="nav-wrapper">
+      <div className="name">ASTARINA MAULIDA</div>
+      <div className="nav">
+        <LinkScroll
+          className="nav-link nav-about"
+          activeClass="active"
+          to="about"
+          spy="true"
+          smooth="true"
+          offset={-70}
+          duration={500}
+        >
+          About
+        </LinkScroll>
+        <LinkScroll
+          className="nav-link nav-projects"
+          activeClass="active"
+          to="projects"
+          spy={true}
+          smooth={true}
+          offset={-70}
+          duration={500}
+        >
+          Projects
+        </LinkScroll>
+        <LinkScroll
+          className="nav-link nav-wireframes"
+          activeClass="active"
+          to="wireframes"
+          spy={true}
+          smooth={true}
+          offset={-70}
+          duration={500}
+        >
+          Wireframes
+        </LinkScroll>
+        <LinkScroll
+          className="nav-link nav-skills"
+          activeClass="active"
+          to="skills"
+          spy={true}
+          smooth={true}
+          offset={-70}
+          duration={500}
+        >
+          Skills
+        </LinkScroll>
+        <a
+          className="nav-link nav-connect"
+          activeClass="active"
+          to="connect"
+          spy={true}
+          smooth={true}
+          offset={-70}
+          duration={500}
+          href="https://www.linkedin.com/in/astarinamaulida/"
+        >
+          Connect
+        </a>
+      </div>
     </nav>
   );
 }
