50689db1f4ce363d17e2a57a25286b3f31e3cbd5
Astarina Maulida <astarina.maulida@gmail.com>
2023-01-04 23:55:06 -0500

    Update year

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
     <div className="footer">
       <footer className="footer">
         <p className="desc">
-          @2022 - Developed and designed by Astarina Maulida
+          @2023 - Developed and designed by Astarina Maulida
         </p>
       </footer>
     </div>
