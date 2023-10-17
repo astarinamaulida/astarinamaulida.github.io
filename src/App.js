406564a01da2322bf3098a67c953440d5948160d
Astarina Maulida <astarina.maulida@gmail.com>
2023-10-17 11:58:13 -0400

    Add new profile pic, add about section, add routes, router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
+import About from "./components/About/About";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
+import { Route, Routes, Link } from "react-router-dom";
 import Homepage from "./components/Homepage/Homepage";
 import "./App.css";
 
@@ -8,9 +10,28 @@ function App() {
   return (
     <React.Fragment>
       <div className="App">
-        <Navbar />
-        <Homepage />
-        <Footer />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Navbar />
+                <Homepage />
+                <Footer />
+              </>
+            }
+          />
+          <Route
+            path="/about"
+            element={
+              <>
+                <Navbar />
+                <About />
+                <Footer />
+              </>
+            }
+          />
+        </Routes>
       </div>
     </React.Fragment>
   );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,44 +5,45 @@ import "./Navbar.css";
 export default function Navbar() {
   return (
     <div className="main-container">
-    <nav className="nav-wrapper">
-      <div className="name">ASTARINA MAULIDA</div>
-      <div className="nav">
-        <LinkScroll
-          className="nav-link nav-about"
-          activeClass="active"
-          to="about"
-          spy="true"
-          smooth="true"
-          offset={-70}
-          duration={500}
-        >
-          Home
-        </LinkScroll>
-        <LinkScroll
-          className="nav-link nav-about"
-          activeClass="active"
-          to="about"
-          spy="true"
-          smooth="true"
-          offset={-70}
-          duration={500}
-        >
-          Work
-        </LinkScroll>
-        <LinkScroll
-          className="nav-link nav-projects"
-          activeClass="active"
-          to="projects"
-          spy={true}
-          smooth={true}
-          offset={-70}
-          duration={500}
-        >
-          About
-        </LinkScroll>
-      </div>
-    </nav>
+      <nav className="nav-wrapper">
+        <div className="name">ASTARINA MAULIDA</div>
+        <div className="nav">
+          <a
+            className="nav-link nav-about"
+            activeClass="active"
+            to="about"
+            spy="true"
+            smooth="true"
+            offset={-70}
+            duration={500}
+          >
+            Home
+          </a>
+          <a
+            className="nav-link nav-about"
+            activeClass="active"
+            to="about"
+            spy="true"
+            smooth="true"
+            offset={-70}
+            duration={500}
+          >
+            Work
+          </a>
+          <a
+            className="nav-link nav-projects"
+            activeClass="active"
+            to="projects"
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={500}
+            href="/About"
+          >
+            About
+          </a>
+        </div>
+      </nav>
     </div>
   );
 }
