6674931663fb8dc3150dc2b67820d4975aa68641
Astarina Maulida <astarina.maulida@gmail.com>
2023-10-19 23:53:22 -0400

    Position

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -4,7 +4,8 @@ import "./Work.css";
 export default function Work() {
   return (
     <div className="main-container">
-      <div className="about-title">Work in Progress</div>
+          <div className="work-title">Work in Progress</div>
+
     </div>
   );
 }
